fb7879dccb5ce3339bec564a53cc6e27ce889366
pppt <pawel.pisarek@smcebi.edu.pl>
2018-10-28 13:17:00 +0100

    Add Game() class

--- snake.py
+++ snake.py
@@ -20,60 +20,83 @@ blue = (0, 0, 255)
 #define snake
 snake_size = 20
 
-def gameLoop():
+class Game():
 
-    gamedead = False
-    head_x = scr_width / 2
-    head_y = scr_height / 2
-    inc_x = 0
-    inc_y = 0
+    def __init__(self):
 
-    while not gamedead:
+        self.state = 1
+        self.game_loop()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gamedead = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    inc_y = snake_size
-                    inc_x = 0
-                elif event.key == pygame.K_UP:
-                    inc_y = -snake_size
-                    inc_x = 0
-                elif event.key == pygame.K_LEFT:
-                    inc_x = -snake_size
-                    inc_y = 0
-                elif event.key == pygame.K_RIGHT:
-                    inc_x = snake_size
-                    inc_y = 0
+    def game_loop(self):
 
-        if head_x >= scr_width or head_x <= 0 or head_y >= scr_height or head_y <= 0:
-            gameOver()
+        while True:
+            if self.state == 1:
+                self.start_game()
+            elif self.state == 2:
+                self.game_over()
+            elif self.state == 3:
+                self.game_exit()
 
-        head_x += inc_x
-        head_y += inc_y
-        screen.fill(black)
-        pygame.draw.rect(screen, red, [head_x, head_y, snake_size, snake_size])
-        pygame.display.update()
+    def start_game(self):
 
-        clock.tick(fps)
+        head_x = scr_width / 2
+        head_y = scr_height / 2
+        inc_x = 0
+        inc_y = 0
 
-def gameOver():
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.state = 3
+                    return None
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        inc_y = snake_size
+                        inc_x = 0
+                    elif event.key == pygame.K_UP:
+                        inc_y = -snake_size
+                        inc_x = 0
+                    elif event.key == pygame.K_LEFT:
+                        inc_x = -snake_size
+                        inc_y = 0
+                    elif event.key == pygame.K_RIGHT:
+                        inc_x = snake_size
+                        inc_y = 0
 
-    while True:
-        screen.fill(white)
-        font = pygame.font.SysFont(None, 25)
-        screen_text = font.render("Press ESC or SPACE", True, red)
-        screen.blit(screen_text, [scr_width/2, scr_height/2])
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    gameLoop()
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    quit()
+            if head_x >= scr_width or head_x <= 0 or head_y >= scr_height or head_y <= 0:
+                self.state = 2
+                return None
 
-gameLoop()
-pygame.quit()
-quit()
+            head_x += inc_x
+            head_y += inc_y
+            screen.fill(black)
+            pygame.draw.rect(screen, red, [head_x, head_y, snake_size, snake_size])
+            pygame.display.update()
+
+            clock.tick(fps)
+
+    def game_over(self):
+
+        while True:
+            screen.fill(white)
+            font = pygame.font.SysFont(None, 25)
+            screen_text = font.render("Press ESC or SPACE", True, red)
+            screen.blit(screen_text, [scr_width/2, scr_height/2])
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.state = 1
+                        return None
+                    if event.key == pygame.K_ESCAPE:
+                        self.state = 3
+                        return None
+                if event.type == pygame.QUIT:
+                    self.state = 3
+                    return None
+
+    def game_exit(self):
+        pygame.quit()
+        quit()
+
+Game()
