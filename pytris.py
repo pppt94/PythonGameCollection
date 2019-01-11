1199871be9f4bb26eb6ea29f3439749f031c825c
pppt <pawel.pisarek@smcebi.edu.pl>
2019-01-11 11:44:12 +0100

    Add Game class & grid function

--- pytris.py
+++ pytris.py
@@ -36,7 +36,51 @@ class Board():
     def __init__(self):
 
         self.grid = []
+        self.blocks_positions = {}
 
-    def draw_grid(self):
+    def create_grid(self):
 
         self.grid = [[(0, 0, 0) for row in range(10)] for column in range(20)]
+
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if (j, i) in self.blocks_positions:
+                    cube = self.blocks_positions[(j, i)]
+                    self.grid[i][j] = cube
+
+        return self.grid
+
+class Game():
+
+    def __init__(self):
+        self.state = 0
+
+    def game_loop(self):
+
+        while True:
+            if self.state == 0:
+                self.start_game()
+            elif self.state == 1:
+                self.game_over()
+            elif self.state == 2:
+                self.help_game()
+            elif self.state == 3:
+                self.state = 0
+                return None
+
+    def start_game(self):
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.state = 3
+                    return None
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        pass
+                    elif event.key == pygame.K_LEFT:
+                        pass
+                    elif event.key == pygame.K_DOWN:
+                        pass
+                    elif event.key == pygame.K_SPACE:
+                        pass
