cedb5a662c049440dd21190588b78354f201063e
pppt <pawel.pisarek@smcebi.edu.pl>
2019-01-12 14:29:28 +0100

    Add check_space function

--- pytris.py
+++ pytris.py
@@ -37,6 +37,20 @@ class Block():
         self.color = colors[shapes.index(self.shape)]
         self.rotation = 0
 
+    def convert_shape(self):
+
+        positions = []
+        format = self.shape[self.rotation % len(self.shape)]
+
+        for i, line in enumerate(format):
+            row = list(line)
+            for j, column in enumerate(row):
+                if column == '0':
+                    positions.append((self.x + j, self.y + i))
+
+        for i, pos in enumerate(positions):
+            positions[i] = (pos[0] - 2, pos[1] - 4)
+
 class Board():
 
     def __init__(self):
@@ -62,6 +76,23 @@ class Board():
             pygame.draw.line(screen, (128, 128, 128), (x_cor, y_cor+i*block_size), (x_cor+300, y_cor+i*block_size))
             for j in range(len(self.grid[i])):
                 pygame.draw.line(screen, (128, 128, 128), (x_cor+j*block_size, y_cor), (x_cor+j*block_size, y_cor+600))
+
+    def check_space(self, shape):
+
+        free_position = []
+        for i in range(20):
+            for j in range(10):
+                if self.grid[i][j] == (0, 0, 0):
+                    free_position.append((j, i))
+
+        conv_shape = shape.convert_shape()
+
+        for position in conv_shape:
+            if position not in free_position:
+                if position[1] > -1:
+                    return False
+
+        return True
 
 class Game():
 
