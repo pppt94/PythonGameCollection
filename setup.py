99d2e063ff81f7e11db8f1b15a1d8c77f74a3302
pppt <pawel.pisarek@smcebi.edu.pl>
2019-01-27 21:51:05 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Projekt_Pygame',
+    version='',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Pawel Pisarek',
+    author_email='pawel.pisarek@smcebi.edu.pl',
+    description=''
+)
